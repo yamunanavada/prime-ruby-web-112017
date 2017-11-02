54a2bda9ca0b4bbe7b9cc2f7a7b25699fd851d27
Yamuna Navada <yamunanavada@gmail.com>
2017-11-02 17:58:26 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
 # Add  code here!
+def prime?(num)
+  range = (2..num-1).to_a
+
+  if num <= 1
+    return false
+  end
+  for i in range
+    if num % i == 0
+      return false
+    end
+
+  end
+
+  true
+
+
+
+
+end
